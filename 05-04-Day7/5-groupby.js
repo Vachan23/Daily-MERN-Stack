400c6536418abb9a1ab80f396db44f83e4340bae
Vachan23 <42373135+Vachan23@users.noreply.github.com>
2022-04-05 18:08:42 +0530

    Update 5-groupby.js

--- 05-04-Day7/5-groupby.js
+++ 05-04-Day7/5-groupby.js
@@ -1,3 +1,18 @@
-let arr = ['vachan', 'yashas', 'pramod', 'darshan', 'ajay', 'vyshak', 'srikant'];
-let b = _.groupBy(arr, "length");
-console.log(b);
+arr=[1,2,3,4,45];
+const groupBy=(callback)=>{
+    var a=[],b=[];
+    arr.map(el=>{
+    if(callback(el)=='less'){
+      a.push(el);
+    }
+    else{
+       b.push(el);
+    }
+})
+console.log(a,b)
+}
+const hs=(el)=>{
+    if(el<5) return "less";
+    else return "more";
+    }
+groupBy(hs);
