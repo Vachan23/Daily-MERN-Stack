549f08f4a659a9d3fa4556871d7efc58e1ca4090
Vachan23 <42373135+Vachan23@users.noreply.github.com>
2022-05-12 09:32:28 +0530

    Delete 10-05-Day29 directory

--- 10-05-Day29/task1/index.js
+++
@@ -1,6 +0,0 @@
-const func = require('./calculator.js');
-
-console.log("sum is", func.add());
-console.log("sub is", func.sub());
-console.log("mult is", func.mult());
-console.log("div is", func.div());
